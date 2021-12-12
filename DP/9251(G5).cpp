cefb8b3a6f3680a2106bcc37043f289c1e0a1bd8
SKW <rjsdntlsdj@gmail.com>
2021-12-12 22:28:51 +0900

    [Add] 9251(G5)

---
+++ DP/9251(G5).cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+using namespace std;
+
+int dp[1001][1001];
+int main() {
+    string s1, s2;
+    cin >> s1 >> s2;
+    for (int i = 1; i <= s1.length(); i++) {
+        for (int j = 1; j <= s2.length(); j++) {
+            if (s1[i - 1] == s2[j - 1])
+                dp[i][j] = dp[i - 1][j - 1] + 1;
+            else
+                dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+        }
+    }
+    cout << dp[s1.length()][s2.length()];
+    return 0;
+}
